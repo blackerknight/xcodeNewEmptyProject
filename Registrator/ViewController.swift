1a2cc6e6de69585fe03de8c25c95516793f7ef2f
Eduardo Mancilla <blackerknight8@gmail.com>
2020-09-12 13:11:58 -0500

    Proyecto recien creado en xcode con backward compatibilidad

---
+++ Registrator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Registrator
+//
+//  Created by eduardo mancilla on 12/09/20.
+//  Copyright © 2020 eduardo mancilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
